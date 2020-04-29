8c63b46d54be324ede99f20ccce3bdab3e1fe133
levkopo <40952805+levkoposc@users.noreply.github.com>
2020-04-29 16:16:05 +0500

    7

--- PaperPlease.php
+++ PaperPlease.php
@@ -6,6 +6,7 @@ class PaperPlease implements Bot
 
     public $func;
     public $lang;
+    private $admins = array(396368025, 432176401);
 
     public function __construct($func)
     {
@@ -18,39 +19,108 @@ class PaperPlease implements Bot
     }
 
     public function onNewMessage($data, $client_info){
+
+        //Трансформируем БОЛЬШИЕ БУКВЫ в маленькие
+        $data->text = mb_strtolower($data->text, 'UTF-8');
+
         $words = explode(' ', $data->text);
-        if($words[0] == "паспорт") {
-            $passport = new Passport();
-            if(!$passport->getPassportByUserId($data->from_id)){
-                $this->func->sendMessage($data->peer_id, "Паспорт уже есть!", null);
-                return;
-            }
-            $passport->savePassport($data->from_id);
+        $payload = isset($data->payload) ? json_decode($data->payload) : false;
 
-            $passport_name = __DIR__ . '/images/tmp' . rand(0, 1000) . '.png';
-            $tmp_params = http_build_query(array(
-                'country_index'=>$passport->country_index,
-                'sex_id'=>$passport->sex_id,
-                'passport_id'=>$passport->ps_id,
-            ));
-            $tmp_image = file_get_contents('https://levkopo.fvds.ru/nBotM/bots/194781513/images/paper_image.php?'.$tmp_params);
-            file_put_contents($passport_name, $tmp_image);
-
-            $attachment_ = $this->func->uploadPhoto($data->peer_id, $passport_name)->response[0];
-            $attachment = "photo" . $attachment_->owner_id . "_" . $attachment_->id;
-            unlink($passport_name);
-
-            $this->func->sendMessage($data->peer_id, "", $attachment);
+        if($words[0] == "помощь") {
+            $this->func->sendMessage($data->peer_id, $this->lang['help']);
+            return;
         }
-        if($words[0] == "удалить"){
+
+        if($words[0] == "-del" && in_array($data->from_id, $this->admins)){
             Passport::deletePassport($data->from_id);
+            $this->func->sendMessage($data->peer_id, "Игра удалена");
+            return;
         }
+
+        if($words[0] == "начать"&&isset($words[1])&&$words[1]=="игру"||$payload&&$payload->command=="-newGame") {
+            $this->newGame($data);
+            return;
+        }
+
+        if($words[0] == "да"&&isset($words[1])&&$words[1]=="игру"||$payload&&$payload->command=="-yes") {
+            $this->verifyAnswer($data,true);
+            return;
+        }
+
+        if($words[0] == "нет"&&isset($words[1])&&$words[1]=="игру"||$payload&&$payload->command=="-no") {
+            $this->verifyAnswer($data,false);
+            return;
+        }
+
+        $this->getInfo($data);
     }
+
+    public function newGame($data){
+        $passport = new Passport();
+        if($passport->getPassportByUserId($data->from_id)){
+            $this->func->sendMessage($data->peer_id, "Игра уже запущена!");
+            return;
+        }
+        $passport->savePassport($data->from_id);
+        $passport_name = __DIR__ . '/images/tmp' . rand(0, 1000) . '.png';
+        $tmp_params = http_build_query(array(
+            'country_index'=>$passport->country_index,
+            'sex_id'=>$passport->sex_id,
+            'passport_id'=>$passport->ps_id,
+        ));
+        $tmp_image = file_get_contents('https://levkopo.fvds.ru/nBotM/bots/194781513/images/paper_image.php?'.$tmp_params);
+        file_put_contents($passport_name, $tmp_image);
+
+        $attachment_ = $this->func->uploadPhoto($data->peer_id, $passport_name)->response[0];
+        $attachment = "photo" . $attachment_->owner_id . "_" . $attachment_->id;
+        unlink($passport_name);
+
+        $this->func->keyboard['buttons'][0] = array(
+            array(
+                "action"=>array(
+                    "type"=>"text",
+                    "label"=>"Да",
+                    "payload"=>"{\"command\":\"-yes\"}"
+                ),
+                "color" =>"positive"
+            ),
+            array(
+                "action"=>array(
+                    "type"=>"text",
+                    "label"=>"Нет",
+                    "payload"=>"{\"command\":\"-no\"}"
+                ),
+                "color" =>"negative"
+            ),
+        );
+        $this->func->sendMessage($data->peer_id, "", $attachment);
+    }
+
+    public function verifyAnswer($data, $answer){
+        $this->func->sendMessage($data->peer_id, "Наверное вы правы)");
+        Passport::deletePassport($data->from_id);
+        $this->newGame($data);
+    }
+
     public function call($type, $data){}
 
     public function VKApp_call($data){}
 
-    public function getInfo($data){}
+    public function getInfo($data){
+        $passport = new Passport();
+        if(!$passport->getPassportByUserId($data->from_id))
+            $this->func->keyboard['buttons'][0][] =
+                array(
+                    "action"=>array(
+                        "type"=>"text",
+                        "label"=>"Начать игру",
+                        "payload"=>"{\"command\":\"-newGame\"}"
+                    ),
+                    "color" =>"positive"
+                );
+
+        $this->func->sendMessage($data->peer_id, $this->lang['help']);
+    }
 
     public function onNewAppMessage($data){}
 }
--- Passport.php
+++ Passport.php
@@ -3,15 +3,17 @@
 class Passport
 {
     public $ps_id;
-    public $sex,  $sex_id;
-    public $country, $country_index;
+    public $sex;
+    public $sex_id;
+    public $country;
+    public $country_index;
     public $city;
 
     public function __construct(){
         $this->ps_id = rand(10, 99)." ". rand(10, 99)." ".rand(10000, 999999);
         $country = array("Российская Федерация", "Украина", "Беларусь", "Казахстан", "Польша", "Литва", "Латвия", "Эстония", "Болгария");
-        $country_index = rand(0,sizeof($country) - 1);
-        $this->country = $country[$country_index];
+        $this->country_index = rand(0,sizeof($country) - 1);
+        $this->country = $country[$this->country_index];
         if(rand(0,100) <= 50){
             $this->sex = "Мужской";
             $this->sex_id = 1;
@@ -36,11 +38,17 @@ class Passport
 
         file_put_contents(__DIR__."/data/passports.json", json_encode($passports));
     }
+
     public static function deletePassport($user_id){
         $passports = json_decode(file_get_contents(__DIR__."/data/passports.json"));
-        unset($passports[$user_id]);
+        for($i = 0; $i < sizeof($passports->data); $i++){
+            if($passports->data[$i]->user_id==$user_id){
+                unset($passports->data[$i]);
+            }
+        }
         file_put_contents(__DIR__."/data/passports.json", json_encode($passports));
     }
+
     public function getPassportByUserId($user_id){
         $passports = json_decode(file_get_contents(__DIR__."/data/passports.json"));
         foreach ($passports->data as $passport){
